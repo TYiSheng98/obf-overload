80dd1dc9f7ddb4a74b2d10e1db95a1a8a4a6b653
Yi Sheng <tys1339@gmail.com>
2022-03-30 00:12:41 +0800

    Change main ->overload methods directly on a given filepath

--- method_overload.py
+++ method_overload.py
@@ -27,29 +27,28 @@ class method_overload():
             data = fp.read()
         return data
 
-    def add_method_overloads(self, smali_files: List[str], class_names_to_ignore: Set[str]):
-        overloaded_method_body = self.get_hardcoded_method_body()
-        added_methods = 0
+    # def add_method_overloads(self, smali_files: List[str], class_names_to_ignore: Set[str]):
+    #     overloaded_method_body = self.get_hardcoded_method_body()
+    #     added_methods = 0
+    #
+    #     # parent directory
+    #     parent = os.path.dirname(smali_files[0])
+    #     output_dir = os.path.join(parent, "2_overload_output_dir")
+    #     try:
+    #         if os.path.isfile(output_dir) is False:
+    #             os.mkdir(output_dir)
+    #             print("created output dir!")
+    #     except FileExistsError:
+    #         print("output dir alr exists!")
+    #
+    #
+    #     for smali_file in smali_files:
+    #         added_methods += self.add_method_overloads_to_file(smali_file, overloaded_method_body,
+    #                                                            class_names_to_ignore, output_dir)
+    #
+    #     print(added_methods, ' new overloaded methods were added in the smali directory')
 
-        # parent directory
-        parent = os.path.dirname(smali_files[0])
-        output_dir = os.path.join(parent, "2_overload_output_dir")
-        try:
-            if os.path.isfile(output_dir) is False:
-                os.mkdir(output_dir)
-                print("created output dir!")
-        except FileExistsError:
-            print("output dir alr exists!")
-
-
-        for smali_file in smali_files:
-            added_methods += self.add_method_overloads_to_file(smali_file, overloaded_method_body,
-                                                               class_names_to_ignore, output_dir)
-
-        print(added_methods, ' new overloaded methods were added in the smali directory')
-
-    def add_method_overloads_to_file(self, smali_filepath: str, overloaded_method_body: str,
-                                     class_names_to_ignore: Set[str], output_dir):
+    def add_method_overloads_to_file(self, smali_filepath: str, class_names_to_ignore: Set[str]):
         new_methods_counter = 0
         self.read_smali_file(smali_filepath)
 
@@ -63,12 +62,14 @@ class method_overload():
             r"(?P<method_return>\S+)",
             re.UNICODE,
         )
+        overloaded_method_body = self.get_hardcoded_method_body()
+
         in_file = StringIO(self.content)
         smali_filename = os.path.basename(smali_filepath)
-        output_filepath = os.path.join(output_dir, smali_filename)
+        # output_filepath = os.path.join(output_dir, smali_filename)
 
         # need to write to same filename when building the apk, added changed_ as poc
-        with open(output_filepath, "w") as out_file:
+        with open(smali_filepath, "w") as out_file:
             skip_remaining_lines = False
             class_name = None
             for line in in_file:
@@ -134,7 +135,9 @@ class method_overload():
                 else:
                     out_file.write(line)
 
-        return new_methods_counter
+        print('{0} new overloaded methods were added in {1}!'.format(new_methods_counter, smali_filepath))
+
+
 
 
 def get_android_class_names():
@@ -149,19 +152,31 @@ def get_random_list_permutations(input_list: list) -> list:
     return permuted_list
 
 
+
+
+
 def main():
     android_class_names = set(get_android_class_names())
-    overloaded = method_overload()
+
     # list_smali_file = ["MainActivity.smali"]
     # list_smali_file = ["before.smali"]
     decompiled_output_path = os.path.join(os.getcwd(), 'app-release\smali\com\example\hello')
     # use the below line if reading from a decompiled directory
-    list_smali_file = [f for f in os.listdir(decompiled_output_path) if f.endswith('.smali')]
-    for k, file in enumerate(list_smali_file):
-        file = os.path.join(decompiled_output_path, file)
-        list_smali_file[k] = file
 
-    overloaded.add_method_overloads(list_smali_file, android_class_names)
+
+    # list_smali_file = [f for f in os.listdir(decompiled_output_path) if f.endswith('.smali')]
+    # for k, file in enumerate(list_smali_file):
+    #     file = os.path.join(decompiled_output_path, file)
+    #     list_smali_file[k] = file
+    #
+    #     # invoke when integrate
+    #     overloaded = method_overload()
+    #     overloaded.add_method_overloads_to_file(list_smali_file, android_class_names)
+
+    # invoke when integrate
+    list_smali_file = r"C:\Users\YISHENG\Desktop\SMALI\MainActivity.smali"
+    overloaded = method_overload()
+    overloaded.add_method_overloads_to_file(list_smali_file, android_class_names)
 
 
 if __name__ == '__main__':
